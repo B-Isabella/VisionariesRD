a2fa70be8d14e16cd33ca7cce20591367b1b6960
B. Isabella Morel <isabellamorelortiz244@gmail.com>
2025-10-04 12:47:51 -0400

    agrege la foto del personaje

--- mainGame.py
+++ mainGame.py
@@ -5,6 +5,17 @@ pygame.init()
 screen = pygame.display.set_mode((1300, 800))
 clock = pygame.time.Clock()
 running = True
+pygame.display.set_caption("NAME OF GAME")
+
+# character
+char_image = pygame.image.load("assets/farmer.png", ).convert_alpha()
+character = pygame.transform.scale(char_image, (100, 110))
+character_x = 600
+character_y = 400
+
+def draw_character():
+    screen.blit(character, (character_x, character_y))
+
 
 while running:
     for event in pygame.event.get():
@@ -13,9 +24,8 @@ while running:
 
     screen.fill("black")
 
-    # RENDER YOUR GAME HERE
-
-    pygame.display.flip()
+    draw_character()
+    pygame.display.update()
 
     clock.tick(60)
 
